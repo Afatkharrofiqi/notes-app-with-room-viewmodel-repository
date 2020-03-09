7dc232a8ca07a7c2190890378dd58d105c74125c
Achmad Fatkharrofiqi <achmad@brainmatics.com>
2020-03-09 22:24:48 +0700

    [ADD] create viewmodel

---
+++ app/src/main/java/com/achmadfatkharrofiqi/notesapp/viewmodel/NoteViewModel.kt
@@ -0,0 +1,28 @@
+package com.achmadfatkharrofiqi.notesapp.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.achmadfatkharrofiqi.notesapp.database.NoteRoomDatabase
+import com.achmadfatkharrofiqi.notesapp.model.Note
+import com.achmadfatkharrofiqi.notesapp.repository.NoteRepository
+import kotlinx.coroutines.launch
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository: NoteRepository
+
+    val allNotes: LiveData<List<Note>>
+
+    init {
+        val noteDao = NoteRoomDatabase.getDatabase(application).noteDao()
+        repository = NoteRepository(noteDao)
+        allNotes = repository.allNotes
+    }
+
+    // do insert note in the background thread
+    fun insert(note: Note) = viewModelScope.launch {
+        repository.insert(note)
+    }
+}
