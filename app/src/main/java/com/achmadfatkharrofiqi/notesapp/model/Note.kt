4f77dbf6b43a5de0ecff10a88a7ab2959229daf1
Achmad Fatkharrofiqi <achmad@brainmatics.com>
2020-03-09 21:59:02 +0700

    [ADD] create new note entity

---
+++ app/src/main/java/com/achmadfatkharrofiqi/notesapp/model/Note.kt
@@ -0,0 +1,8 @@
+package com.achmadfatkharrofiqi.notesapp.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "note_table")
+class Note(@PrimaryKey @ColumnInfo(name = "note") val note: String)
